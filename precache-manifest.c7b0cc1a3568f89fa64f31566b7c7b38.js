33ac4e7b35c32baa638030cfa935fd6ee21ad419
Kyle Hoernlein <kyle.hoernlein@verizonwireless.com>
2020-04-13 16:01:00 -0400

    Updates

--- precache-manifest.c7b0cc1a3568f89fa64f31566b7c7b38.js
+++ precache-manifest.c7b0cc1a3568f89fa64f31566b7c7b38.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "85878e744ee534ea31881551f5048976",
+    "revision": "f22ef85a1549002a553faac6d53b5b4d",
     "url": "/android-testing/index.html"
   },
   {
-    "revision": "abdda963d71acc4696a0",
+    "revision": "4c229bce35173f60a0d1",
     "url": "/android-testing/static/css/main.1e17f288.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/android-testing/static/js/2.9898899f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "abdda963d71acc4696a0",
-    "url": "/android-testing/static/js/main.3e4793b0.chunk.js"
+    "revision": "4c229bce35173f60a0d1",
+    "url": "/android-testing/static/js/main.ac35ffaf.chunk.js"
   },
   {
     "revision": "9a4e2a7812cf2831c2a9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/android-testing/precache-manifest.0c162c7bfa4efb9cb830cd20da396e73.js"
+  "/android-testing/precache-manifest.c7b0cc1a3568f89fa64f31566b7c7b38.js"
 );
 
 self.addEventListener('message', (event) => {
